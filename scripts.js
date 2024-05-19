61ffb5a52cc888bd44c1d54b2df700e6b317d04d
unknown <rafliega770@gmail.com>
2024-05-19 23:40:49 +0700

    uploud belajar JS

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.writeln("HELLO WORLD")
+document.writeln("</br>")
+document.writeln(0x22)
+document.writeln("</br>")
+document.writeln("name: " + "Rafli\n")
+document.writeln("</br>")
+
+//escape sequence
+document.writeln("\"Rafli Hasyim\"\n")
+document.write('\'gahas\'\n')
+document.writeln("</br>")
+
+var namaSaya =  "hantu"
+var namaSaya =  "dia"
+document.writeln(namaSaya)
+document.writeln("</br>")
+
+//operator aritmatika
+let angka = 100 + 100
+document.write(angka)
+document.writeln("</br>")
+
+//operator augmented assignment
+let bir = 500
+document.write(bir)
